a131d37629c9eec1ffa4953997c73b568e01c3e8
quqiao <553248560@.com>
2020-06-28 17:36:55 +0800

    20200628 爬虫平台添加hezongyy中的selenium和mysql处理

--- target_page/web_page/hezongyy_py.py
+++ target_page/web_page/hezongyy_py.py
@@ -6,59 +6,84 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+import pymysql
 
-executable_path="C:/Users/Administrator/AppData/Local/Google/Chrome/Application/chromedriver.exe"
-
-# def get_html_page(url):
-#    try:
-#        driver = webdriver.Chrome(executable_path=executable_path)
-#        driver.get("https://www.hezongyy.com/auth/login")
-#        time.sleep(2)
-#        username = driver.find_element_by_name('user_name')
-#        password = driver.find_element_by_name('password')
-#        username.send_keys('测试05')
-#        password.send_keys('123456')
-#        # 模拟点击“登录”按钮
-#        driver.find_element_by_class_name('login').click()
-#        time.sleep(1)
-#        driver.get(url)
-#        time.sleep(3)  # 停顿3秒等待页面加载完毕！！！（必须留有页面加载的时间，否则获得的源代码会不完整。）
-#        html_sourcode = driver.page_source
-#        driver.close()
-#        return html_sourcode
-#    except Exception as e:
-#        print(e)
-#
-# def crawl_page(url):
-#     html_doc = get_html_page(url)
-#     soup = BeautifulSoup(html_doc, 'lxml')
-
+list_jiage = []
+list_mingzi = []
+list_compamy = []
+list_guige = []
+list_xiaoqi = []
+executable_path = "C:/Users/Administrator/AppData/Local/Google/Chrome/Application/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=executable_path)
-driver.get("https://www.hezongyy.com/auth/login")
-time.sleep(2)
-username = driver.find_element_by_name('user_name')
-password = driver.find_element_by_name('password')
-username.send_keys('测试05')
-password.send_keys('123456')
-# 模拟点击“登录”按钮
-driver.find_element_by_class_name('login').click()
-time.sleep(1)
-driver.get("https://www.hezongyy.com/puyao.html")
-time.sleep(3)  # 停顿3秒等待页面加载完毕！！！（必须留有页面加载的时间，否则获得的源代码会不完整。）
-html_sourcode = driver.page_source
-driver.close()
-soup = BeautifulSoup(html_sourcode, 'lxml')
-# jg = soup.find_all(attrs={'class': {'datu-jiage'}})
-jg = soup.find_all(class_="datu-jiage")
-mz = soup.find_all(class_="datu-mingzi")
-list1 = []
-for i in jg:
-    list1.append(i.text)
-list2 = []
-for t in mz:
-    list2.append(t.text)
-dataframe = pd.DataFrame({'a_name': list1, 'b_name': list2})  # 字典中的key值即为csv中列名
-dataframe.to_csv("test-01.csv", index=False, sep=',')  # 将DataFrame存储为csv,index表示是否显示行名，default=True
+def login_hezongyy():
+    driver.get("https://www.hezongyy.com/auth/login")
+    time.sleep(2)
+    username = driver.find_element_by_name('user_name')
+    password = driver.find_element_by_name('password')
+    username.send_keys('测试05')
+    password.send_keys('123456')
+    # 模拟点击“登录”按钮
+    driver.find_element_by_class_name('login').click()
+
+def crawl_hezongyy(count):
+    time.sleep(1)
+    driver.get("https://www.hezongyy.com/puyao.html?order=DESC&pageNumber=%d" % count)
+    time.sleep(3)  # 停顿3秒等待页面加载完毕！！！（必须留有页面加载的时间，否则获得的源代码会不完整。）
+    html_sourcode = driver.page_source
+    driver.close()
+    soup = BeautifulSoup(html_sourcode, 'lxml')
+    # jg = soup.find_all(attrs={'class': {'datu-jiage'}})
+    jg = soup.find_all(class_="datu-jiage")
+    mz = soup.find_all(class_="datu-mingzi")
+    cj = soup.find_all(class_="datu-compamy")
+    gg = soup.find_all(class_="datu-guige")
+    xq = soup.find_all(class_="datu-xiaoqi")
+    for i in jg:
+        list_jiage.append(i.text)
+    for t in mz:
+        list_mingzi.append(t.text)
+    for j in cj:
+        list_compamy.append(j.text)
+    for g in gg:
+        list_guige.append(g.text)
+    for x in xq:
+        list_xiaoqi.append(x.text)
+
+
+def save_csv():
+    dataframe = pd.DataFrame({'价格': list_jiage, '药名': list_mingzi})  # 字典中的key值即为csv中列名
+    dataframe.to_csv("test-01.csv", index=False, sep=',')  # 将DataFrame存储为csv,index表示是否显示行名，default=True
+
+def save_mysql():
+    conn = pymysql.connect('localhost', 'root', '123456', 'spider_platform')  # 有中文要存入数据库的话要加charset='utf8'
+    # 创建游标
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("DROP TABLE IF EXISTS hezongyy_py")
+    # 使用预处理语句创建表
+    sql = """
+          CREATE TABLE hezongyy_py 
+          (
+             ID int unsigned auto_increment primary key,
+             name VARCHAR(100),
+             cj VARCHAR(100),
+             gg VARCHAR(100),
+             xq VARCHAR(100),
+             price VARCHAR(100)
+          )
+          """
+    cursor.execute(sql)
+    insert_sql = """
+                 insert into hezongyy_py 
+                 (name,cj,gg,xq,price) 
+                 VALUES
+                 (%s,%s,%s,%s,%s)
+                 """
+    for i in range(len(list_jiage)):
+        cursor.execute(insert_sql, (list_mingzi[i], list_compamy[i], list_guige[i], list_xiaoqi[i], list_jiage[i]))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 
 
